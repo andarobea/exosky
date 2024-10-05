c75bd2bc3d458448629a1eb81ff716e8316d8903
Panait Teodor Dimitrie <teodordpanait+GH@gmail.com>
2024-10-05 17:46:46 +0300

    program.py now can request starts with non-null parallax; the magnitude is a wip

--- program.py
+++ program.py
@@ -4,31 +4,34 @@
 
 from astroquery.gaia import Gaia
 import time 
-SIZEOF_REQUEST = 2000 # trebuie marit dar momentan il las asa sa ruleze mai rapid
+SIZEOF_REQUEST = 1000                           # trebuie marit dar momentan il las asa sa ruleze mai rapid
 INITIAL_TIME = time.time()
 
 query = """
-SELECT TOP """ + str(SIZEOF_REQUEST) + """ SOURCE_ID, RA, DEC, PARALLAX
+SELECT TOP """ + str(SIZEOF_REQUEST) + """ SOURCE_ID, RA, DEC, PARALLAX, phot_g_mean_mag
 FROM gaiadr3.gaia_source
+WHERE PARALLAX IS NOT NULL
+AND phot_g_mean_mag < 20.5
 """
 
 job = Gaia.launch_job(query)
 
 results = job.get_results()
 
-print("Time passed: " + str(time.time() - INITIAL_TIME))
+# print("Time passed: " + str(time.time() - INITIAL_TIME))
 
 number_of_rows = len(results)
-print(f"Number of rows: {number_of_rows}")
+# print(f"Number of rows: {number_of_rows}")
 
 
-print(f"{'Source ID':<20} {'RA (deg)':<15} {'Dec (deg)':<15} {'Parallax (mas)':<15}")
+print(f"{'RA'},{'Dec'},{'Parallax'},{'Phot_g_mean_mag'}")
 
 # Print a separator
-print('-' * 65)
+# print('-' * 65)
 
 # Print each row in the results
 for row in results:
-    print(f"{row['SOURCE_ID']:<20} {row['RA']:<15.6f} {row['DEC']:<15.6f} {row['PARALLAX']:<15.6f}")
+    print(f"{row['SOURCE_ID']},{row['RA']:.6f},{row['DEC']:.6f},{row['PARALLAX']:.6f},{row['phot_g_mean_mag']:.6f}")
+    # print(f"{row['RA']:.6f},{row['DEC']:.6f}")
 
-print("Time after print: " + str(time.time() - INITIAL_TIME))
+# print("Time after print: " + str(time.time() - INITIAL_TIME))
